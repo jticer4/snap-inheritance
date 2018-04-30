f8496d8b3af1d3f0abbbbd37322acac751a73b23
James Ticer <jticer4@icloud.com>
2018-04-30 09:17:12 -0600

    added my 3 classes, added their getter and setter methods, constructors, created instances of all my classes, and then echoed the state variables for each

---
+++ index.php
@@ -0,0 +1,94 @@
+<?php
+
+class vehicle {
+	public $licensePlate;
+
+	/**
+	 * vehicle constructor.
+	 * @param string $licensePlate
+	 */
+	public function __construct(string $licensePlate) {
+		$this->licensePlate = $licensePlate;
+	}
+
+	/**
+	 * @return string
+	 **/
+	public function getLicensePlate(): string {
+		return $this->licensePlate;
+	}
+
+	/**
+	 * @param string $licensePlate
+	 **/
+	public function setLicensePlate($licensePlate): void {
+		$this->licensePlate = $licensePlate;
+	}
+
+}
+
+class nissanAltima extends vehicle {
+	public $belongsToMarlon;
+
+	/**
+	 * nissanAltima constructor.
+	 * @param $belongsToMarlon
+	 **/
+	public function __construct($belongsToMarlon) {
+		parent::__construct("licensePlate");
+		$this->belongsToMarlon = $belongsToMarlon;
+	}
+
+	/**
+	 * @return boolean
+	 **/
+	public function getBelongsToMarlon(): bool {
+		return $this->belongsToMarlon;
+	}
+
+	/**
+	 * @param boolean $belongsToMarlon
+	 **/
+	public function setBelongsToMarlon($belongsToMarlon): void {
+		$this->belongsToMarlon = $belongsToMarlon;
+	}
+
+}
+
+class horseAndBuggy extends vehicle {
+	public $numHorses;
+
+	/**
+	 * horseAndBuggy constructor.
+	 * @param $numHorses
+	 */
+	public function __construct($numHorses) {
+		parent::__construct("licensePlate");
+		$this->numHorses = $numHorses;
+	}
+
+	/**
+	 * @return integer
+	 */
+	public function getNumHorses(): int {
+		return $this->numHorses;
+	}
+
+	/**
+	 * @param integer $numHorses
+	 */
+	public function setNumHorses($numHorses): void {
+		$this->numHorses = $numHorses;
+	}
+
+}
+
+$toyota = new vehicle("54854858");
+echo "This Toyota's license plate number is " . $toyota->getLicensePlate() . "<br>";
+
+$honda = new nissanAltima("false");
+echo "Does this honda belong to Marlon? " . $honda->belongsToMarlon . "<br>";
+
+$wagon = new horseAndBuggy(5);
+echo "I have a wagon, it has " . $wagon->getNumHorses() . " horses";
+
